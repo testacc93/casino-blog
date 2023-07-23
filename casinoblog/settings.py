b54b21672e4d948ceb84e3b0631ee1001f35f422
Ankur Kumar <bitcoinwallet02525@gmail.com>
2023-07-23 15:30:11 +0530

    allowed hosts all and debug flase

--- casinoblog/settings.py
+++ casinoblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6am2!3gx(n+tzxfumszk$---_$b!ru!c!tc=h(qy8zp_fjc4v5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
